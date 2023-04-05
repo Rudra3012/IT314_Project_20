60840d3dd0193ede78c89bc95e6b23a5ad6b5ef4
Rudra Gohel <202001178@daiict.ac.in>
2023-04-05 22:46:15 +0530

    Manual crossword css page created.

--- code/IT314_Project_20/crosswordApp/components/create_crossword.py
+++ code/IT314_Project_20/crosswordApp/components/create_crossword.py
@@ -1,18 +1,20 @@
 from django_unicorn.components import UnicornView
 import CrosswordGenerator as cg
-from crosswordApp.Classes.Crossword import Crossword , gridCell
+from crosswordApp.Classes.Crossword import Crossword, gridCell
 from pymongo import MongoClient
 import json
+
 client = MongoClient('mongodb+srv://Group20:g7uxB5fMdWcstCt4@cluster0.zjgczqo.mongodb.net/?retryWrites=true&w=majority')
-db=client['CrossWordManagement']
+db = client['CrossWordManagement']
+
 
 class CreateCrosswordView(UnicornView):
-    title: str=""
-    description: str=""
-    word: str=""
-    clue: str=""
+    title: str = ""
+    description: str = ""
+    word: str = ""
+    clue: str = ""
     clues_and_words = []
-    crossword_grid=[]
+    crossword_grid = []
 
     def add_clue_and_word(self):
         print(f'add clue and word: {self.clue} {self.word}')
@@ -21,12 +23,12 @@ class CreateCrosswordView(UnicornView):
         self.clue = ""
 
     def reset_page(self):
-        self.title=""
-        self.description=""
-        self.word=""
-        self.clue=""
+        self.title = ""
+        self.description = ""
+        self.word = ""
+        self.clue = ""
         self.clues_and_words = []
-        self.crossword_grid=[]
+        self.crossword_grid = []
 
     def generate(self):
         print("Generate crossword")
@@ -36,42 +38,44 @@ class CreateCrosswordView(UnicornView):
 
         words = [word for clue, word in self.clues_and_words]
 
-        grid=cg.generate_crossword(words)
+        grid = cg.generate_crossword(words)
 
         print(grid)
-        row=[]
-
+        row = []
+        self.crossword_grid = []
         for i in range(len(grid)):
-            if grid[i]!='\n':
+            if grid[i] != '\n':
                 row.append(grid[i])
             else:
                 self.crossword_grid.append(row)
-                row=[]
+                row = []
 
         print(self.crossword_grid)
 
     def save_crossword(self):
-            # print("Save crossword")
-            # print("Title: " + self.title)
-            # print("Description: " + self.description)
-            # print("Words and clues: " + str(self.clues_and_words))
-            # print("Crossword grid: " + str(self.crossword_grid))
-            grid_dict = {}
-            gridList= []
-            crosswordStr= ""
-            words = [word.upper() for clue, word in self.clues_and_words]
-            for i in range(len(self.crossword_grid)):
-                for j in range(len(self.crossword_grid[i])):
-                    grid_dict[(i,j)] = self.crossword_grid[i][j]
-                    crosswordStr+=self.crossword_grid[i][j]
-                    gridList.append(gridCell(i,j,self.crossword_grid[i][j]).__dict__)
-                crosswordStr+='\n'
-            print(words)
-            print(crosswordStr)
-            wordsHor, wordsVer = cg.getWordOrient(crosswordStr, words)
-            print(f'wordsHor: {wordsHor}')
-            print(f'wordsVer: {wordsVer}')
-            collections = db['crosswordApp_crossword']
-            newCrossword = Crossword(title=self.title, description=self.description, width=len(self.crossword_grid[0]), height=len(self.crossword_grid),  cluesUp=[], cluesDown=[],WordsUp=wordsVer,WordsDown=wordsHor, gridList=gridList)
-            print(newCrossword.__dict__)
-            collections.insert_one(newCrossword.__dict__)
+        # print("Save crossword")
+        # print("Title: " + self.title)
+        # print("Description: " + self.description)
+        # print("Words and clues: " + str(self.clues_and_words))
+        # print("Crossword grid: " + str(self.crossword_grid))
+        grid_dict = {}
+        gridList = []
+        crosswordStr = ""
+        words = [word.upper() for clue, word in self.clues_and_words]
+        for i in range(len(self.crossword_grid)):
+            for j in range(len(self.crossword_grid[i])):
+                grid_dict[(i, j)] = self.crossword_grid[i][j]
+                crosswordStr += self.crossword_grid[i][j]
+                gridList.append(gridCell(i, j, self.crossword_grid[i][j]).__dict__)
+            crosswordStr += '\n'
+        print(words)
+        print(crosswordStr)
+        wordsHor, wordsVer = cg.getWordOrient(crosswordStr, words)
+        print(f'wordsHor: {wordsHor}')
+        print(f'wordsVer: {wordsVer}')
+        collections = db['crosswordApp_crossword']
+        newCrossword = Crossword(title=self.title, description=self.description, width=len(self.crossword_grid[0]),
+                                 height=len(self.crossword_grid), cluesUp=[], cluesDown=[], WordsUp=wordsVer,
+                                 WordsDown=wordsHor, gridList=gridList)
+        print(newCrossword.__dict__)
+        collections.insert_one(newCrossword.__dict__)
