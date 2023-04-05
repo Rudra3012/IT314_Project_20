eab9d18914f62f2656420cc48e3935d1cdce56f0
jainam <jainambhavsar2108@gmail.com>
2023-04-05 23:58:52 +0530

    sidebar

--- code/IT314_Project_20/crosswordApp/static/HomePage/index.js
+++ code/IT314_Project_20/crosswordApp/static/HomePage/index.js
@@ -2,6 +2,7 @@ let margin = 250
 
 $(document).ready(function () {
     $('#sidebarCollapse').on('click', function () {
+        $('#sidebar').toggleClass('active');
         $('#content').css('margin-left', `${margin}px`);
         $('.white-section').css('margin-left', `${margin}px`);
         margin = 250 - margin;
