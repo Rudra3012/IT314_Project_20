8a82ddfc367cbb8dcb70d76748945ce98674283c
Jill208 <202001181@daiict.ac.in>
2023-04-14 23:37:57 +0530

    Created forgot password functionality in login page and Email notification

---
+++ code/IT314_Project_20/crosswordApp/helper.py
@@ -0,0 +1,40 @@
+from django.core.mail import send_mail
+from datetime import datetime
+from django.utils import timezone
+
+from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import render
+
+
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+
+from django.conf import settings
+def send_forget_password_mail(email , token ):
+    subject = 'Your forget password link'
+    message = f'Hi , click on the link to reset your password http://127.0.0.1:8000/change-password/{token}/'
+    email_from = settings.EMAIL_HOST_USER
+    recipient_list = [email]
+    send_mail(subject, message, email_from, recipient_list)
+    return True
+
+def send_mail(receiver_address, subject, mail_content):
+    sender_address = 'crosswordmanagement@gmail.com'  # add sender's email address here
+    sender_pass = 'zkgxfsduhuvebiki'  # add sender's passsword address here
+
+    message = MIMEMultipart()
+    message['From'] = sender_address
+    message['To'] = receiver_address
+    message['Subject'] = subject
+
+    message.attach(MIMEText(mail_content, 'plain'))
+
+    session = smtplib.SMTP('smtp.gmail.com', 587)
+    session.starttls()
+    session.login(sender_address, sender_pass)
+    text = message.as_string()
+    session.sendmail(sender_address, receiver_address, text)
+    session.quit()
