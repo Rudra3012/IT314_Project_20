8fa3e09fa4a154c219909f8b6e54ef536df918c4
Rudra Gohel <202001178@daiict.ac.in>
2023-04-28 12:32:58 +0530

    Added messages in registration page.

--- code/IT314_Project_20/venv/Scripts/activate.bat
+++ code/IT314_Project_20/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\aashk\Desktop\New folder\clone2\IT314_Project_20\code\IT314_Project_20\venv
+set VIRTUAL_ENV=C:\Users\rudra\Documents\DAIICT\sem 6\Software Engineering\github\IT314_Project_20\code\IT314_Project_20\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
